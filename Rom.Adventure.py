bcb14e4e639308f32a26b5d07ff387280ff64111
BenniBaka <bensko12@vgs.nfk.no>
2023-09-20 14:57:17 +0200

    Update Rom.Adventure.py

--- Rom.Adventure.py
+++ Rom.Adventure.py
@@ -1,4 +1,5 @@
-hp = 100
+trust = 40
+Chadness = 10
 def rom_1():
     print("du er på skeld og Red sier at han så deg vent selv om det var du som så han vent, og han kaller deg får en sussy baka, og nå er alle i crewet enig med red. hva gjør du?")
     valg = input("A: Løpe vekk for og prøve og gjømme deg B: Si, Red kinda SUS C: Gå til huset til Red og skru av nettet til han. -> ")
@@ -16,10 +17,8 @@ def rom_4():
     print("God ide, men møte gikk ut av tid mens du var på vei og du ente opp med og DØ!")
 
 def rom_3():
-    print("Alle ente opp med og vote Red og nå er det bare en imposter igjen. Nå er møte over og nå vet alle at du er snill\
-som betyr at den andre imposteren kommer til og prøve og drepe deg. Hva gjør du nå?")
-    valg = input("A: Du holer deg med 2 eller flere crewmates B: Du prøver og løpe så langt vekk som mulig fra de andre, og du prøver\
-og gjømme deg C: Du går til kjelleren til Shaman Durek for og spørre om guidence D: Du ringer Truls og Caller et emergency meeting. -> ")
+    print("Alle ente opp med og vote Red og nå er det bare en imposter igjen. Nå er møte over og nå vet alle at du er snill, som betyr at den andre imposteren kommer til og prøve og drepe deg. Hva gjør du nå?")
+    valg = input("A: Du holer deg med 2 eller flere crewmates B: Du prøver og løpe så langt vekk som mulig fra de andre, og du prøverog gjømme deg C: Du går til kjelleren til Shaman Durek for og spørre om guidence D: Du ringer Truls og Caller et emergency meeting. -> ")
     if valg == "A":
         rom_5()
     if valg == "D":
@@ -44,10 +43,10 @@ def rom_8():
 def rom_6():
     print("Du setter på høyrtaller slilk at Truls kan høre alle stemmene i møte, Truls gjennskjenner all stemmene siden han stalker alle barna i byen, Truls\
 vet hvor alle barna bor så du får han til og løpe til alle kidsan sine hus for og og se hvem som er imposteren og siden det er kids dobbler han løpefarten sin som er like raskt som the flash\
-og så kom han tilbake på 0,593 sekunder, men ingen av dem var imposteren, men det var en person han ikke hadde løpt til, og det var Svart AKA Shaman durek,\
+og så kom han tilbake på 3,14 sekunder, men ingen av dem var imposteren, men det var en person han ikke hadde løpt til, og det var Svart AKA Shaman durek,\
 han hadde ikke løpt dit fordi han er dypt inni jungelen i Congo. og siden alle andre er crewmates må shamanen skjølv være imposteren.")
     valg = input("imposteren var Black/Shaman Durek. Hva gjør du nå. A: Du sier black kinda SUS B: Du sier at vi burde gjøre ferdig tasks\
-    C: Du suer Shaman durek for illegal drug business med the cartel der han har solgt rundt 50Kg av 100% pure colombian cokecaine og rundt 20kg av black tar heroin og hannes beste seller 100kg 99,67% pure crystal meth som har tatt heisenberg ut av business. -> ")
+C: Du suer Shaman durek for illegal drug business med the cartel der han har solgt rundt 50Kg av 100% pure colombian cokecaine og rundt 20kg av black tar heroin og hans beste seller 100kg 99,67% pure crystal meth som har tatt heisenberg ut av business. -> ")
     if valg == "A":
         rom_9()
     if valg == "B":
@@ -59,16 +58,161 @@ def rom_9():
     print("Du sier black kinda sus, og alle i hele game kaller deg for en rasict fordi det er 2023 og du blir cancelled på alle sosiale media appene du har")
 
 def rom_10():
-    print("Alle skipper og dere fortsetter og gjøre tasks")
+    print("Alle skipper og dere fortsetter og gjøre tasks.")
+    print("Det er 2 tasks igjen men en av de døde står AFK og han har de 2 tasksan igjen så dere kan ikke vinne på tasks og reactor ble sabotert.")
+    print("Hva gjør du nå?")
+    valg = input("A: du leter helt til du finner en kropp. B: du du fikser reactor. C: Du ringer den som ikke har gjort tasksan sine på mobilen. D: Du infiltrerer basen til shaman durek.")
+    if valg == "A":
+        rom_16()
+    if valg == "B":
+        rom_17()
+    if valg == "C":
+        rom_18()
+    if valg == "D":
+        rom_19()
+
+
+def rom_16():
+    print("Du leter og leter, men finner ingen kropp helt til du ble en kropp.")
+
+
+def rom_17():
+    print("Du går til reactor og fikser den mens imposteren fikser glocken, PANG!")
+    print("Shaman kom bakfra med glocken.")
+
+
+def rom_18():
+    print("Du ringer grønn og sier at han må gjøre tasksan sine")
+    print("nå gjør han tasksan sine, men det er forskjent, reactor ble ikke fiksa")
+
+
+def rom_19():
+    print("Du ringer opp Roger sitt crew og sier at de må bli med til Congo for og få takk i Shaman Durek, og at de kommer til og bli rik av det.")
+    print
 
 
 def rom_11():
-    print("Du ringer opp crewet til Roger som består av Roger, Håvard-baka og Truls og du får dem til og tracke lokasjonen til shaman durek, men de kalrte ikke det, fordi det går ikke ann og tracke lokkasjonen til shaman durek, viss ikke man bruker unethical ways.\
+    print("Du ringer opp crewet til Roger som består av Roger, Håvard-baka, Truls og slavene deres og du får dem til og tracke lokasjonen til shaman durek, men de klarte ikke det, fordi det går ikke ann og tracke lokkasjonen til shaman durek, viss ikke man bruker unethical ways.\
 hva gjør du nå?")
-    input("A: du går inn i en dark alley for og finne en local crack-head for og høre hvor han for drugsan sine fra B: du repoterer Shaman durek til govermentet av congo. C: du challenger shaman durek til hand to hand combat. Du ringer shaman durek på discord. -> ")
+    valg = input("A: du går inn i en dark alley for og finne en local crack-head for og høre hvor han for drugsan sine fra B: du repoterer Shaman durek til govermentet av congo. C: du challenger shaman durek til hand to hand combat. D: Du ringer shaman durek på discord. -> ")
+    if valg == "A":
+        rom_12()
+    if valg == "B":
+        rom_13()
+    if valg == "C":
+        rom_14()
+    if valg == "D":
+        rom_15()
+
 
 def rom_12():
-    pass
+    print("Du går ned mellom husene til osama bin ladin og nikocado acocado og ser en svart skillese i mørke og det ser ut som om han sniffer meth")
+
+def rom_13():
+    print("De leter i to uker men finner ingen hint om shaman durek, men shaman durek finnner deg.")
+
+def rom_14():
+    print("Siden shaman durek har en stor banan så har han ingen valg anna enn og akseptere challengen din.")
+
+def rom_15():
+    print("Du ringer shaman banan")
+    print("Han svarer, hva gjør du nå?")
+    valg = input("A: Du tracker hans eksakte lokasjon via og se hvor dekningen hans kommer fra og du sender Truls sine slaver for og ta han ned. B: Du sier at han må komme til Sindres magiske kjeller. C: Du sier at han må komme på besøk for og få en suprise.\
+D: Du sier at han må møte Gustavo for og conducte business med los buornos hermanos og at Gus sier at han må kjappe seg")
+    if valg == "A":
+        rom_20()
+    if valg == "B":
+        rom_21()
+    if valg == "C":
+        rom_22()
+    if valg == "D":
+        rom_23()
+    
+
+def rom_20():
+    print("Man kan ikke ikke tracke lokasjonen til Shaman Durek med og finne ut av hvor dekningen hans kommer i fra fordi han bruker ikke dekning han bruker potetsalat som wifi.")
+    
+
+def rom_21():
+    global trust
+    print("Shaman Durek lurer på hvorfor.")
+    print("Hva skal du si?")
+    valg = input("A: Du sier at Sindre kunne bare gi informasjonen til Shaman Durek og at du måtte komme til basemanten hannes får og finne ut. B: Du sier at han må komme for og starte opp en Minecraft server som kommer til og gjøre dem rik.\
+C: Du sier at han Sindre har lyst til at Shaman og han skal joine Håvard sin kjeller -> ")
+    if valg == "A":
+        trust -= 30
+    print("1 hour later.")
+    print(f"Nå er shaman durek kommet fram til Sindre's magiske kjeller og du har {trust} fra Shaman Durek")
+    valg = input("")
+
+    if valg == "B":
+        trust += 10
+    print("1 hour later.")
+    print(f"Nå er shaman durek kommet fram til Sindre's magiske kjeller og du har {trust} trust igjen")
+    valg = input("")
+
+    if valg == "C":
+        trust -= 10
+    print("1 hour later.")
+    print(f"Nå er shaman durek kommet fram til Sindre's magiske kjeller og du har {trust} trust igjen")
+    valg = input("")
+
+def rom_24():
+    print
+
+
+def rom_22():
+    global trust
+    trust -= 20
+    print("Han aksepterer og komme på besøk og han er på vei")
+    valg = input
+
+
+def rom_23():
+    global trust
+    trust += 30
+    print(f"Han er kjapt på vei og du har {trust} trust.")
+    print ("Mens han er på vei ringer du politiet og sier at Shaman Durek er på vei, Du må ta deg et våpen viss han angriper deg. HVa velger du?")
+    valg = input("A: Kort kniv. B: kjøkken kniv. C: spade. D: baseball-kølle. E: Bue med 5 piler. F: Knyttnever")
+    if valg == "A":
+        rom_Kort_kniv()
+    if valg == "B":
+        rom_Kjøkken_kniv()
+    if valg == "C":
+        rom_Spade()
+    if valg == "D":
+        rom_Baseball_kølle()
+    if valg == "E":
+        rom_Pil_og_bue()
+    if valg == "F":
+        rom_knyttnever()
+
+
+def rom_Kort_kniv():
+    print
+
+def rom_Kjøkken_kniv():
+    print
+
+def rom_Spade():
+    print
+
+def rom_Baseball_kølle():
+    print
+
+def rom_Pil_og_bue():
+    print
+
+def rom_knyttnever():
+    global Chadness
+    Chadness += 50
+    print
+
+
+
+
+
+
 
 
 
@@ -76,5 +220,3 @@ def rom_12():
 
 
 rom_1()
-if hp <=0:
-    print("Shit, du døde, Skill-isue")
